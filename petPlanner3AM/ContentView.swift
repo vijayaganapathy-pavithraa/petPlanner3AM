8bf781d08449b2f81f6a5d55dfbfbfcfa2d9a680
vijayaganapathy-pavithraa <145333042+vijayaganapathy-pavithraa@users.noreply.github.com>
2024-07-20 09:47:41 +0800

    Initial Commit

---
+++ petPlanner3AM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  petPlanner3AM
+//
+//  Created by Vijayaganapathy Pavithraa on 20/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ petPlanner3AM/petPlanner3AMApp.swift
@@ -0,0 +1,17 @@
+//
+//  petPlanner3AMApp.swift
+//  petPlanner3AM
+//
+//  Created by Vijayaganapathy Pavithraa on 20/7/24.
+//
+
+import SwiftUI
+
+@main
+struct petPlanner3AMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
